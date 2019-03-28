d06774d87a87e851da0b58c31872f768fb043193
MaximMaximus <42582464+MaximMaximus@users.noreply.github.com>
2019-03-28 20:41:41 +0100

    Create makes10.h

---
+++ Übungen/Übungsblatt 1/makes10.h
@@ -0,0 +1,17 @@
+// Given 2 ints, a and b, output "Makes 10!" if one if them is 10 or if their sum is 10. Complete the following program:
+
+#include <stdio.h>
+
+int main(void) {
+
+  	int a, b;
+
+	scanf("%d %d", &a, &b);
+
+	if(a == 10 || b == 10 || a+b==10)
+	{
+		printf("Makes 10!\n");
+	} 
+
+  return 0;
+}
