705f8641c6b80ac1731848625b993bc7eda05a79
MaximMaximus <42582464+MaximMaximus@users.noreply.github.com>
2019-04-06 15:59:20 +0200

    Create ln2iter.c

---
+++ Übungen/Übungsblatt 2/Zusatz/ln2iter.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int wertX = 2;
+    
+    float endwert = 0.00;
+    float i = 0;
+    float j = 2;
+    for(i=1; endwert <= wertX; i++)
+    {
+        if(wertX == 1) return 1;
+        else {
+            endwert = endwert + (1 / i) - ( 1 / j);
+            printf("%f\n", endwert);
+        }
+        j+=2;
+    }
+    
+    printf("Ihre Eingabe ist: %i\n", wertX);
+    printf("Der Wert %i wird ab den Wert %.0f ueberstiegen!\n", wertX, i);
+
+    return 0;
+}
