f88dcb7107bbeb85f90ba94100db5de82d1d3ff3
MaximMaximus <42582464+MaximMaximus@users.noreply.github.com>
2019-04-13 11:35:00 +0200

    Create reverse.c

---
+++ CAlsErsteProgrammiersprache/reverse.c
@@ -0,0 +1,16 @@
+/* 9.6.7.3 Seite 214 Datei: revers.c */
+#include <stdio.h>
+
+int main (void)
+{
+   char text1 [4] = "EIN";
+   char text2 [4];
+   int index1, index2;
+   for (index2 = 0, index1 = 2; index1 >= 0; index2++, index1--)
+   {
+      text2 [index2] = text1 [index1];
+   }
+   text2 [index2] = '\0';
+   printf ("\n%s\n%s\n", text1, text2);
+   return 0;
+}
