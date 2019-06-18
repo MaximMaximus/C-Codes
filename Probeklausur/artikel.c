346bd804f980690e7405e2df43730ad06462b8a4
MaximMaximus <42582464+MaximMaximus@users.noreply.github.com>
2019-06-18 18:09:44 +0200

    Update artikel.c

--- Probeklausur/artikel.c
+++ Probeklausur/artikel.c
@@ -16,48 +16,65 @@ struct Bewertung {
 	struct Bewertung *next;
 };
 
-void anlegen(struct Artikel *art, int artikelnummer, char *artText) {
-/* 	int i = 0;
-	for ( i = 0; i < MAX_ARTIKEL; i++ ) {
-		if (art[i][0].artnr == 0 && art[i][0].bezeichnung == '\0') {
-			art[i][0].artnr = artikelnummer;
-			strcpy(art[i][0].bezeichnung, artText);
-		}
-	} */
+void anlegen(struct Artikel **anchor, int artikelnummer, char *artText) {
 	int i = 0;
-	for ( i = 0; i < MAX_ARTIKEL; i++ ) {
-		art[i] = (struct Artikel*)malloc(sizeof(struct Artikel));
-		if (art->artnr == 0 && art->bezeichnung == '\0') {
-			art->artnr = artikelnummer;
-			strcpy(art->bezeichnung, artText);
+	while(anchor[i] != NULL) {
+		i++;
+	}
+	anchor[i] = (struct Artikel*)malloc(sizeof(struct Artikel));
+	anchor[i]->artnr = artikelnummer;
+	anchor[i]->bezeichnung = malloc( strlen(artText)+1 );
+	strcpy(anchor[i]->bezeichnung, artText);
+}
+
+void bewerten(struct Artikel **anchor, int nr, char *bewertungstext, int sterne) {
+	struct Bewertung *neu = (struct Bewertung*)malloc(sizeof(struct Bewertung));
+	neu->text = malloc( strlen(bewertungstext)+1 );
+	strcpy(neu->text, bewertungstext);
+	neu->punkte = sterne;
+	int i = 0;
+	while(anchor[i] != NULL) {
+		if(anchor[i]->artnr == nr) {
+			neu->next = anchor[i]->bewertungen;
+			anchor[i]->bewertungen = neu;
 		}
-		art++;
+		i++;
 	}
 }
 
-void bewerten(struct Artikel *artikel, int nr, char *bewertungstext, int sterne) {
+void anzeigen(struct Artikel **artikel) {
 	int i = 0;
-	for ( i = 0; i < MAX_ARTIKEL; i++ ) {
-		if (artikel[i].artnr == nr) {
-			strcpy(artikel[i].bewertungen->text, bewertungstext);
-			artikel[i].bewertungen->punkte = sterne;
-			//artikel[i]bewertungen->next = (*artikel)->bewertungen;
+	int j = 0;
+	while(artikel[i] != NULL) {
+		printf("Artikel: %d - %s\n", artikel[i]->artnr, artikel[i]->bezeichnung);
+		
+		while(artikel[i]->bewertungen != NULL) {
+			printf("%*c", -9+artikel[i]->bewertungen->punkte, ' ');
+			for(j = 0; j < artikel[i]->bewertungen->punkte; j++) {
+				printf("*");
+			}
+			printf(" %s\n", artikel[i]->bewertungen->text);
+			artikel[i]->bewertungen = artikel[i]->bewertungen->next;
 		}
+		printf("\n");
+		i++;
 	}
 }
-
 
 int main() {
+
 	struct Artikel *artikel[MAX_ARTIKEL] = { NULL };
 
-	anlegen(artikel, 12345, "Hörbuch Biene Maja");
-	//bewerten(artikel, 12345, "Höre ich zum Einschlafen", 4);
-	//bewerten(artikel, 12345, "Mehr Handlung!", 1);
+	anlegen(artikel, 12345, "Hoerbuch Biene Maja");
+	bewerten(artikel, 12345, "Hoere ich zum Einschlafen", 4);
+	bewerten(artikel, 12345, "Mehr Handlung!", 1);
 
-	//anlegen(artikel, 4549, "Smartphone Universum III");
-	//bewerten(artikel, 4549, "Voll das krasse Handy", 5);
 
-	//anzeigen(artikel);
+	anlegen(artikel, 4549, "Smartphone Universum III");
+	bewerten(artikel, 4549, "Voll das krasse Handy", 5);
+
+	anzeigen(artikel);
+	anzeigen(artikel);
 
 	return 0;
 }
