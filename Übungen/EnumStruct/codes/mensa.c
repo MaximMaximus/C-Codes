c3ecd0ba4f12b0ed36f8d37a22b0fb743f0c43c6
MaximMaximus <42582464+MaximMaximus@users.noreply.github.com>
2019-06-25 18:50:14 +0200

    Add files via upload

---
+++ Übungen/EnumStruct/codes/mensa.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum Kennzeichnungen { VEGAN, VEGETARISCH, GLUTENFREI, ALLERGENFREI } Kennzeichnungen;
+
+typedef struct Gericht {
+	int kcal;
+	char *name;
+	Kennzeichnungen inhalt;
+	float preis;
+	int Speisennummer;
+	struct Gericht *next;
+} Gericht;
+
+Gericht *create(int kcal, char *name, Kennzeichnungen inh, float geld, int nr) {
+	Gericht *neu = calloc(1, sizeof(Gericht)); //(Gericht*)malloc(sizeof(Gericht));
+	if ( neu == NULL ) return NULL;
+	neu->kcal = kcal;
+	neu->name = malloc(strlen(name)+1);
+	strcpy(neu->name, name);
+	neu->inhalt = inh;
+	neu->preis = geld;
+	neu->Speisennummer = nr;
+	return neu;
+}
+
+int foodcmp(Gericht *g1, Gericht *g2) {
+	if(g1->kcal == g2->kcal) {
+		return strcmp(g1->name, g2->name);
+	}
+	if(g1->kcal < g2->kcal) return -1;
+	else if(g1->kcal > g2->kcal) return 1;
+	else return 0;
+}
+
+void add_sorted(Gericht **anchor, Gericht *neu) {
+	printf("Add sorted: %s\n", neu->name);
+	while(*anchor != NULL && (foodcmp(*anchor, neu) < 0 )) {
+		anchor = &(*anchor)->next;
+	}
+	neu->next = *anchor;
+	*anchor = neu;
+}
+
+void print_all(Gericht *anchor) {
+	char *kenn[] = { "Vegan", "Vegetarisch", "Glutenfrei", "Allergenfrei" };
+	printf("Print list: \n");
+	Gericht *temp = anchor;
+	while(temp != NULL) {
+		printf("%s - %.2f Geld (%s, %d kcal, Speisennummer: %d)\n", temp->name, temp->preis, kenn[temp->inhalt], temp->kcal, temp->Speisennummer );
+		temp = temp->next;
+	}
+}
+
+int main() {
+	Gericht *liste = NULL;
+	
+	add_sorted( &liste, create(1, "Schockolade", GLUTENFREI, 50, 20) );
+	add_sorted( &liste, create(20, "Reis", VEGAN, 3.5, 1) );
+	add_sorted( &liste, create(50, "Kuh-Milch", ALLERGENFREI, 100, 5) );
+	add_sorted( &liste, create(20, "Chemicalie-X", VEGETARISCH, 2, 10) );
+	print_all(liste);
+
+	return 0;
+}
